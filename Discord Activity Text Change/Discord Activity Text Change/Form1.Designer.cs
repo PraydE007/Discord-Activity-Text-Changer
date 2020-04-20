b34569893424a68dad384e7580810ece7f3683e3
PraydE007 <prayde.niste@gmail.com>
2020-04-20 20:50:08 +0300

    v0.4

--- Discord Activity Text Change/Discord Activity Text Change/Form1.Designer.cs
+++ Discord Activity Text Change/Discord Activity Text Change/Form1.Designer.cs
@@ -35,13 +35,16 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button1.Location = new System.Drawing.Point(12, 50);
+            this.button1.Location = new System.Drawing.Point(12, 150);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 38);
             this.button1.TabIndex = 0;
@@ -53,7 +56,7 @@
             // 
             this.button2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button2.Location = new System.Drawing.Point(109, 50);
+            this.button2.Location = new System.Drawing.Point(109, 150);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(91, 38);
             this.button2.TabIndex = 1;
@@ -65,7 +68,7 @@
             // 
             this.button3.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button3.Location = new System.Drawing.Point(297, 50);
+            this.button3.Location = new System.Drawing.Point(297, 150);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(91, 38);
             this.button3.TabIndex = 2;
@@ -78,7 +81,7 @@
             this.textBox1.BackColor = System.Drawing.SystemColors.Window;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(12, 112);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(376, 32);
             this.textBox1.TabIndex = 3;
@@ -90,13 +93,28 @@
             this.notifyIcon1.Text = "Open DS Changer";
             this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click_1);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(150, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.BackColor = System.Drawing.SystemColors.ControlText;
-            this.ClientSize = new System.Drawing.Size(400, 99);
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -112,6 +130,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Discord Activity Text Changer";
             this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,6 +145,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Discord Activity Text Change/Discord Activity Text Change/Form1.cs
+++ Discord Activity Text Change/Discord Activity Text Change/Form1.cs
@@ -15,34 +15,62 @@ namespace Discord_Activity_Text_Change
 
         private Point pos;
 
+        private int x = 0;
+        private int y = 0;
+
         public Form1()
         {
             InitializeComponent();
+            timer1.Interval = 1;
         }
 
+        // Exit Button
         private void button3_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
+        // Apply New Text
         private void button1_Click(object sender, EventArgs e)
         {
             this.Text = textBox1.Text;
         }
 
+        // Hide Window
         private void button2_Click(object sender, EventArgs e)
         {
             pos = this.Location;
-            this.Location = new Point(-500, -200);
-            //this.WindowState = FormWindowState.Minimized;
+            this.Location = new Point(-500, -300);
             notifyIcon1.Visible = true;
         }
 
         private void notifyIcon1_Click_1(object sender, EventArgs e)
         {
             this.Location = pos;
-            //this.WindowState = FormWindowState.Normal;
             notifyIcon1.Visible = false;
+        }
+
+        // Start Moving
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                x = MousePosition.X - this.Location.X;
+                y = MousePosition.Y - this.Location.Y;
+                timer1.Enabled = true;
+            }
+        }
+
+        // Stop Moving
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        // Window Moving Func
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Location = new Point(MousePosition.X - x, MousePosition.Y - y);
         }
     }
 }
